a4a25466848003cc1ae22e082a3bd73760641498
sarahabing <abingsa1@gmail.com>
2023-11-13 11:25:26 -0700

    the webhook actually does something

--- server.js
+++ server.js
@@ -4,17 +4,18 @@ const crypto = require('crypto');
 const app = express();
 app.use(express.json());
 
-app.post('/webhook', (req, res) => {
-  const payload = JSON.stringify(req.body);
-  const sig = 'sha1=' + crypto.createHmac('sha1', process.env.WEBHOOK_SECRET).update(payload).digest('hex');
+app.post('/', (req, res) => {
+  const payload = req.body;
+  // const sig = 'sha1=' + crypto.createHmac('sha1', process.env.WEBHOOK_SECRET).update(payload).digest('hex');
+  // if (req.headers['x-hub-signature'] === sig) {
+    // const alert = req.action
 
-  if (req.headers['x-hub-signature'] === sig) {
-    const alert = req.body?.alert;
+    // if (alert) {
 
-    if (alert) {
-      console.log('Code scanning alert status:', alert.status);
-    }
-  }
+      console.log('Code scanning alert action:', payload.action);
+      // console.log('code scanning alert actual alert', req.alert.url);
+    // }
+  // }
 
   res.status(200).end();
 });
